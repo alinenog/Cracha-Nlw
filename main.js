3077afd1b90580c292e56691fa37886ca9a50254
Aline Nogueira <61569421+alinenog@users.noreply.github.com>
2021-10-28 21:09:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+/*criação objeto com os dados que substituiirão os links anteriores*/
+
+const LinksSocialMedia = {
+  github: 'alinenog',
+  youtube: 'rocketseat',
+  instagram: 'aline_noog10',
+  facebook: 'AlineNog10',
+  twitter: 'rocketseat'
+}
+/*Criando a função*/
+function chageSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+/*Chamando a função*/
+chageSocialMediaLinks()
+
+/*Trocando os links via API */
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  /*Vai até a Url pega a informação e traz de volta*/
+  fetch(url)
+    /*PROMESSA é a resposta quando da certo o retorno */
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
